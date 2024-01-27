adb9de5f5e0618fe42e6d65f402f1cf3d5e54b55
lungowe_s <lungowe.simasiku1@gmail.com>
2024-01-27 14:38:59 +0200

    Lost track of the changes I made. However, these are the last styles leading to the completion of the project

--- public/js/about.js
+++ public/js/about.js
@@ -1,3 +1,25 @@
+// JavaScript code for the hamburger menu.
+const mobileMenu = document.querySelector(".hamburger__menu")
+const navMenu = document.querySelector(".nav__menu")
+const navButton = document.querySelector(".nav__button")
+const navIcon = document.querySelector('.icon')
+
+mobileMenu.addEventListener("click", event => {
+    navIcon.classList.toggle("open")
+    if (navIcon.classList.contains("open")) {
+        navIcon.src = "/assets/icons/close.svg"
+    } else {
+        navIcon.src = "/assets/icons/hamburger.svg"
+    }
+
+    navMenu.classList.toggle("nav__open")
+    navButton.classList.toggle("active")
+
+   
+})
+
+
+// JavaScrip code for the accordion
 const accordions = Array.from(document.querySelectorAll(".accordion"))
 const accordionIndicator = document.querySelectorAll(".accordion__indicator");
 
