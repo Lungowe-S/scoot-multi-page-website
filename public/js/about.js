da7f00e8f4bdcb6a8880e8e220a902c3f0a436ee
lungowe_s <lungowe.simasiku1@gmail.com>
2024-02-28 22:36:26 +0200

    Added a mobile-hero class to the hero section of all the pages to ensure the is a grayscale overlay across the hero when the hamburger menu is clicked on.

--- public/js/about.js
+++ public/js/about.js
@@ -3,17 +3,21 @@ const mobileMenu = document.querySelector(".hamburger__menu")
 const navMenu = document.querySelector(".nav__menu")
 const navButton = document.querySelector(".nav__button")
 const navIcon = document.querySelector('.icon')
+const hero = document.querySelector('.mobile-hero')
 
 mobileMenu.addEventListener("click", event => {
     navIcon.classList.toggle("open")
     if (navIcon.classList.contains("open")) {
         navIcon.src = "/assets/icons/close.svg"
+        hero.classList.add('overlay')
     } else {
         navIcon.src = "/assets/icons/hamburger.svg"
+        hero.classList.remove('overlay')
     }
 
     navMenu.classList.toggle("nav__open")
     navButton.classList.toggle("active")
+    //hero.classList.add('overlay')
 
    
 })
@@ -31,5 +35,6 @@ accordions.forEach(accordion => {
     accordionHeader.addEventListener("click", event => {
         //Toggle the is-open class
         accordion.classList.toggle("is-open")
+        
     })
 })
